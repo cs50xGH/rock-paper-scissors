d911d1eb85c611d482cfb4780f22f40d78082178
jj <fatflick123@gmail.com>
2022-08-10 15:16:49 -0700

    fix the rock paper scissors round

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
  //Create a function called getComputerChoice that randomly returns R,P,S
  function getComputerChoice() {
-    const choice = ["rock","paper","scissor"];
+    const choice = ["rock","paper","scissors"];
     const random = choice[Math.floor(Math.random() * choice.length )];
     return random;
  }
@@ -8,16 +8,27 @@
  //Write a fucntion that plays a single round of R,P,S
 
  function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "paper") {
-       return text = "You lose, paper beats rock";
-    }else if (playerSelection != "scissor") {
-    
-
-  }
-}
+   if (playerSelection === "rock" && computerSelection === "paper") {
+      return `You lose rock is defeated by paper`;
+   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      return `You win ${playerSelection} beats ${computerSelection}`;
+   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+      return `You win ${playerSelection} beats ${computerSelection}`;
+   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      return `You lose rock beats scissors`;
+   } else if (playerSelection === "paper" && computerSelection === "rock") {
+      return "You win paper beats rock";
+   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+      return "you lose scissors beats paper";
+   } else (playerSelection === computerSelection); {
+      return `Tie! ${playerSelection} equals ${computerSelection}`;
+   }
+ }
    
-  const playerSelection = "rock";
+  const playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
   const computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
+
+  console.log(computerSelection)
   
-  let userAnswer = prompt('Choose rock, paper, or scissor.');
+  
