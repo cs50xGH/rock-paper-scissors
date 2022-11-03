60029f3cc2461a8ec7d0518c5aac2a230c2960d7
jj <fatflick123@gmail.com>
2022-11-02 22:01:16 -0700

    restored files

--- javascript.js
+++ javascript.js
@@ -1,60 +1,42 @@
- //Create a function called getComputerChoice that randomly returns R,P,S
  function getComputerChoice() {
-    const choice = ["rock","paper","scissors"];
+    const choice = ['rock','paper','scissors'];
     const random = choice[Math.floor(Math.random() * choice.length )];
     return random;
  }
  
- //Write a fucntion that plays a single round of R,P,S
- //done
+
 
  function playRound() {
-   const playerSelection = prompt("Choose rock, paper, or scissors").toLowerCase();
-   const computerSelection = getComputerChoice();
-   if (playerSelection === "rock" && computerSelection === "paper") {
-      return `You lose paper beats rock`;
-   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-      return `You win rock beats scissors`;
-   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-      return `You win scissors beats paper`;
-   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-      return `You lose rock beats scissors`;
-   } else if (playerSelection === "paper" && computerSelection === "rock") {
-      return "You win paper beats rock";
-   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-      return "You lose scissors beats paper";
-   } else (playerSelection === computerSelection); {
-      return `Tie! ${playerSelection} equals ${computerSelection}`;
-   }
- }
-   
+   let computerSelection = getComputerChoice()
 
-  
-  
-  function game() {
-   let playerScore = 0;
-   let computerScore = 0;
-   let roundResult = playRound();
-
-      for (let i = 0; i < 5; i++) {
-         playRound();
-         if (roundResult === 'You win rock beats scissors' || roundResult === 'You win paper beats rock' || roundResult === 'You win scissors beats paper') {
-            ++playerScore;
-          } else if (roundResult === 'You lose paper beats rock' || roundResult === 'You lose scissors beats paper' || roundResult === 'You lose rock beats scissors') {
-            ++computerScore
-            }
+      if (this.textContent == 'rock' && computerSelection == 'scissors' ||
+          this.textContent == 'paper' && computerSelection == 'rock' ||
+          this.textContent == 'scissors' && computerSelection == 'paper') {
+      result.textContent = `It's a win ${this.textContent} beats ${computerSelection}`;
+      }else if (this.textContent == computerSelection) {
+            result.textContent = `it's a tie ${this.textContent} equals ${computerSelection}`;
+      }else {
+            result.textContent = `${this.textContent} loses against ${computerSelection}`;
+          };
       }
-      if (playerScore > computerScore) {
-         return ('You win ' + playerScore + ' to ' + computerScore + '.');
-       } else if (playerScore < computerScore) {
-         return ('You lose ' + computerScore + ' to ' + playerScore + '.');
-       } else {
-         return 'It was a tie, you both won the same number of rounds.';
-       }
-     
-     } 
-   
 
-   console.log(game())
+ 
+
   
+const result = document.querySelector('.result') 
   
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+
+
+const array = [rock, paper, scissors];
+for (let i = 0; i < array.length; i++) {
+   array[i].addEventListener('click', playRound);
+};
+
+  
+
+
+   
